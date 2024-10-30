3568757d951e2398187e870e57baf131f8893748
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2024-10-30 15:40:00 +0100

    first project add

--- portfolio/my-gatsby-site/src/pages/projects/index.js
+++ portfolio/my-gatsby-site/src/pages/projects/index.js
@@ -2,19 +2,24 @@ import * as React from "react"
 import { Link, graphql } from "gatsby"
 import Layout from "../../components/layout"
 import Seo from "../../components/seo"
-
+import { StaticImage } from "gatsby-plugin-image"
 const BlogPage = ({ data }) => {
   return (
-    <Layout pageTitle="My Projects">
-      
+    <Layout>
       {data.allMdx.nodes.map(node => (
         <article key={node.id}>
           <h2>
             <Link to={`/projects/${node.frontmatter.slug}`}>
               {node.frontmatter.title}
+              <StaticImage
+            alt="Progman"
+            src={node.frontmatter.hero_image}
+            className=""
+          /> 
             </Link>
           </h2>
           <p>Posted: {node.frontmatter.date}</p>
+          <p>Role: {node.frontmatter.role}</p>
         </article>
       ))}
     </Layout>
@@ -29,6 +34,7 @@ export const query = graphql`
           date(formatString: "MMMM D, YYYY")
           title
           slug
+          role
         }
         id
       }
