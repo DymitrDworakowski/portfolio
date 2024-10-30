8fd36da1753bb71cdd7934f6b5e8085537dc9daa
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2024-10-30 13:01:20 +0100

    about json add

--- portfolio/my-gatsby-site/src/pages/about.js
+++ portfolio/my-gatsby-site/src/pages/about.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { Trans } from "gatsby-plugin-react-i18next"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import { graphql } from "gatsby"
@@ -6,8 +7,8 @@ import {
   aboutContainer,
   sectionTitle,
   aboutText,
-  connectText
-} from "./about.module.css";
+  connectText,
+} from "./about.module.css"
 
 const AboutPage = () => {
   return (
@@ -15,41 +16,48 @@ const AboutPage = () => {
       <div className={aboutContainer}>
         <span className={sectionTitle}>My Journey</span>
         <p className={aboutText}>
-          Hi! I'm Dymitr Dworakowski, Junior Full-Stack Developer. I've always
-          been passionate about technology and design, and I knew from my first
-          projects that I wanted to create beautiful and functional web
-          applications.
+          <Trans i18nKey="journey">
+            Hi! I'm Dymitr Dworakowski, Junior Full-Stack Developer. I've always
+            been passionate about technology and design, and I knew from my
+            first projects that I wanted to create beautiful and functional web
+            applications.
+          </Trans>
         </p>
-
         <span className={sectionTitle}>What I Focus On</span>
         <p className={aboutText}>
-          I like to work on projects that combine a user-friendly interface and
-          powerful functionality. I believe that good design is not only about
-          aesthetics, but also about ease of use.
+          <Trans i18nKey="focus">
+            I like to work on projects that combine a user-friendly interface
+            and powerful functionality. I believe that good design is not only
+            about aesthetics, but also about ease of use.
+          </Trans>
         </p>
-
         <span className={sectionTitle}>Skills & Tools</span>
         <p className={aboutText}>
-          I use HTML5, CSS3, JavaScript and React to create efficient and modern
-          interfaces. I also work with Node.js and MongoDB, which allows me to
-          work on the full application stack.
+          <Trans i18nKey="tools">
+            I use HTML5, CSS3, JavaScript and React to create efficient and
+            modern interfaces. I also work with Node.js and MongoDB, which
+            allows me to work on the full application stack.
+          </Trans>
         </p>
 
         <span className={sectionTitle}>What Inspires Me</span>
         <p className={aboutText}>
-          I draw inspiration from design and user feedback. Every new project is
-          a chance to learn something new and improve my skills.
+          <Trans i18nKey="inspires">
+            I draw inspiration from design and user feedback. Every new project
+            is a chance to learn something new and improve my skills.
+          </Trans>
         </p>
 
         <span className={sectionTitle}>Let’s Connect!</span>
         <p className={`${aboutText} ${connectText}`}>
+          <Trans i18nKey="connect"></Trans>
           I am always open to new ideas and cooperation. If you have an
           interesting project or a question, I would be happy to chat!
         </p>
       </div>
     </Layout>
-  );
-};
+  )
+}
 
 export const query = graphql`
   query ($language: String!) {
@@ -63,8 +71,8 @@ export const query = graphql`
       }
     }
   }
-`;
+`
 
-export const Head = () => <Seo title="About Me" />;
+export const Head = () => <Seo title="About Me" />
 
-export default AboutPage;
+export default AboutPage
