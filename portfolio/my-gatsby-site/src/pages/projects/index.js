5cc9371fac39cebd297af220b9ad4172e2f95862
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2024-10-31 12:41:58 +0100

    add projects

--- portfolio/my-gatsby-site/src/pages/projects/index.js
+++ portfolio/my-gatsby-site/src/pages/projects/index.js
@@ -3,24 +3,28 @@ import { Link, graphql } from "gatsby"
 import Layout from "../../components/layout"
 import Seo from "../../components/seo"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
-import { image,project } from "./index.module.css"
+import { image,project} from "./index.module.css"
 
 const BlogPage = ({ data }) => {
   return (
     <Layout>
       {data.allMdx.nodes.map(node => (
+        
         <article key={node.id} className={project}>
+           <Link to={`/projects/${node.frontmatter.slug}`}>
           <div className={image}>
           <h2 >
-            <Link to={`/projects/${node.frontmatter.slug}`}>
+           
               {node.frontmatter.title}
               <GatsbyImage image={getImage(node.frontmatter.hero_image)} alt={node.frontmatter.hero_image_alt} />
-            </Link>
+            
           </h2>
           </div>
           <p>Posted: {node.frontmatter.date}</p>
           <p>Role: {node.frontmatter.role}</p>
+          </Link>
         </article>
+      
       ))}
     </Layout>
   )
