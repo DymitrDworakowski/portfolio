ad00cb337e14603fed134bf2990fcf0bba6c8ddc
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2024-11-04 13:37:36 +0100

    fix

--- portfolio/my-gatsby-site/src/components/layout.js
+++ portfolio/my-gatsby-site/src/components/layout.js
@@ -1,61 +1,19 @@
 // src/components/Layout.js
 import React from "react"
-import { Link, Trans } from "gatsby-plugin-react-i18next"
-import Navbar from "./Navbar";
+import Navbar from "./Navbar"
 import ThemeToggle from "./ThemeToggle"
 import LanguageSwitcher from "./LanguageSwitcher"
 import Footer from "./Footer"
-import {
-  heading,
-  navLinks,
-  navLinkItem,
-  navLinkText,
-  siteTitle,
-} from "./layout.module.css"
 
-const Layout = ({ pageTitle, children }) => {
+const Layout = ({ children }) => {
   return (
     <div className="container">
       <Navbar />
       <ThemeToggle />
       <LanguageSwitcher />
-      {/* <header className="row">
-        <div className="column">
-          <h1 className={siteTitle}>
-            <Trans>My Portfolio</Trans>
-          </h1>
-         
-        </div>
-        <nav className="column-2">
-          <ul className={navLinks}>
-            <li className={navLinkItem}>
-              <Link to="/" className={navLinkText}>
-                <Trans>Home</Trans>
-              </Link>
-            </li>
-            <li className={navLinkItem}>
-              <Link to="/about" className={navLinkText}>
-                <Trans>About</Trans>
-              </Link>
-            </li>
-            <li className={navLinkItem}>
-              <Link to="/projects" className={navLinkText}>
-                <Trans>Projects</Trans>
-              </Link>
-            </li>
-            <li className={navLinkItem}>
-              <Link to="/certificates" className={navLinkText}>
-                <Trans>Certificates</Trans>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </header> */}
-      
+
       <main className="column">
-        <div className="row">
-          {children}
-        </div>
+        <div className="row">{children}</div>
       </main>
 
       <footer className="row">
