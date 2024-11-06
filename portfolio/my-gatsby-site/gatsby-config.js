c86a27a07648c7f1b7ace604c3b7d12d52ce1851
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2024-11-06 19:14:10 +0100

    fix project

--- portfolio/my-gatsby-site/gatsby-config.js
+++ portfolio/my-gatsby-site/gatsby-config.js
@@ -63,7 +63,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/emile-perron-xrVDYZRGdw4-unsplash.jpg`, // This path is relative to the root of the site.
+        icon: `./static/images/favicon.jpeg` // This path is relative to the root of the site.
       },
     },
   ],
