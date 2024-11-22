6e963f79755e1bf5cdbc5dc371a98ff856efe523
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2024-11-22 10:49:04 +0100

    add change lau for certificates page

--- portfolio/my-gatsby-site/src/pages/certificates.js
+++ portfolio/my-gatsby-site/src/pages/certificates.js
@@ -1,36 +1,53 @@
 import * as React from "react"
-import { Link } from "gatsby"
-
+import { graphql, Link } from "gatsby";
+import { useTranslation } from "gatsby-plugin-react-i18next"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const Certificates = () => (
+const Certificates = () => {
+  const { t } = useTranslation(["certificates"])
+  console.log(t("certificates:title")); // Має повернути значення
+  return(
   <Layout>
     <div>
-    <h1>My certificates</h1>
-    <div
-      style={{
-        display: "grid",
-        gridTemplateColumns: "repeat(auto-fit, minmax(400px, 1fr))",
-        gap: "20px",
-      }}
-    >
-      <iframe
-        src="/certificates/Dymitr Dworakowski.pdf"
-        title="Сертифікат 1"
-        width="100%"
-        height="500px"
-        style={{ border: "1px solid #ccc", borderRadius: "8px" }}
-      ></iframe>
-    </div>
+      <h1>{t("certificates:title")}</h1>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fit, minmax(400px, 1fr))",
+          gap: "20px",
+        }}
+      >
+        <iframe
+          src="/certificates/Dymitr Dworakowski.pdf"
+          title="Сертифікат 1"
+          width="100%"
+          height="500px"
+          style={{ border: "1px solid #ccc", borderRadius: "8px" }}
+        ></iframe>
+      </div>
 
-    <Link to="/" style={{ marginTop: "20px", display: "inline-block" }}>
-    Return to the main page
-    </Link>
+      <Link to="/" style={{ marginTop: "20px", display: "inline-block" }}>
+      {t("certificates:back")}
+      </Link>
     </div>
   </Layout>
 )
+}
 
+export const query = graphql`
+ query ($language: String!) {
+  locales: allLocale(filter: { language: { eq: $language } }) {
+    edges {
+      node {
+        ns
+        data
+        language
+      }
+    }
+  }
+}
+`
 export const Head = () => <Seo title="Certificates" />
 
 export default Certificates
