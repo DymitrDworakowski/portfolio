d8c550b57f734aa9d4552d48e3cdfd2a6c3503b4
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2025-06-16 15:48:03 +0200

    css fix

--- portfolio/my-gatsby-site/src/components/TechBadge.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { badge } from "./TechBadge.module.css"
-
-const TechBadge = ({ technology }) => (
-  <span className={badge}>{technology}</span>
-)
-
-export default TechBadge
